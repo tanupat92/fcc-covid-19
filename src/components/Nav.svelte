68e23fdb9656fb82242a1b46c5b10c02fd7b94e5
Tanupat Boon <tanupat92@gmail.com>
2022-09-13 19:10:21 -0400

    navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,41 @@
 <script>
-	//export let segment;
+	let isOpen = false; 
+	export let segment;
+	function toggleNav() {
+		isOpen = !isOpen; 
+	}
 </script>
 
+<style>
+	@media(min-width: 769px){
+		nav {
+			border-bottom: solid gray 1px; 
+		}
+	}
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation" >
+	<div class="navbar-brand">
+		<a href="/" class="navbar-item">
+			<img src="logo.jpeg" alt="virus">
+		</a>
+		<span
+			class="navbar-burger"
+			class:is-active={isOpen}
+			on:click={toggleNav}
+			aria-label="menu"
+			aria-expanded="false"
+		>
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+
+		</span>
+	</div>
+	<div class="navbar-menu" class:is-active={isOpen}>
+		<div class="navbar-start">
+			<a href="/" class="navbar-item" class:is-active={segment === undefined}>Home</a>
+			<a href="/about" class="navbar-item" class:is-active={segment === undefined}>About</a>
+		</div>
+	</div>
+</nav>
