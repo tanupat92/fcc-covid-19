8c13935d4771b57056ee282dde92d4e223e85ee4
Tanupat Boon <tanupat92@gmail.com>
2022-09-13 18:48:34 -0400

    pages and components

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import CovidStat from "../components/CovidStat.svelte";
+    import CovidChart from "../components/CovidChart.svelte";
+    import TableContainer from "../components/TableContainer.svelte";
+</script>
+
+<CovidStat />
+<CovidChart />
+<TableContainer />
